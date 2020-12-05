5e9b63a7dc074d2812f8f8660003e29ec3618571
kenderb <kenderbolivar@gmail.com>
2020-12-05 12:36:18 -0500

    Add some object to display

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -5,7 +5,25 @@ import React from 'react';
 function getTitle(title) {
   return title;
 }
-const myArray = ['kender', 'Bolivar', 'one more', 4, 5]
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 const welcome = {
   greeting: 'Hey',
@@ -17,13 +35,24 @@ function App() {
     <div>
       <img src={logo} alt="react logo" className="App-logo"/>
       <h1>
-        {welcome.greeting} {welcome.title}
+        My Hacker Stories
       </h1>
-      <ul>
-        { myArray.map(number => {return <li>{number}</li> })}
-      </ul>
       <label htmlFor="search">Search: </label>
       <input type="text" id="search"/>
+
+      <hr />
+      {list.map(item => {
+        return(
+          <div key={ item.objectID }> 
+            <span>
+              <a href={item.url}>{item.title} </a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </div>
+        );
+      })}
     </div>
   );
 }
