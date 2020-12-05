b1f6691d6e911ce22931d18507444598cd1113e4
kenderb <kenderbolivar@gmail.com>
2020-12-05 13:06:04 -0500

    Create List component

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -63,22 +63,24 @@ function App() {
       </h1>
       <label htmlFor="search">Search: </label>
       <input type="text" id="search"/>
-
       <hr />
-      {list.map(item => {
-        return(
-          <div key={ item.objectID }> 
-            <span>
-              <a href={item.url}>{item.title} </a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </div>
-        );
-      })}
+      <List />
     </div>
   );
 }
 
+function List() {
+  return list.map(function(item) {
+    return (
+        <div key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+        </div>
+      );
+  });
+}
 export default App;
