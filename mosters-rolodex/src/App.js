25c1c2d9cde10a59d49eeb2fcae2ed9bdf0d9b52
kenderb <kenderbolivar@gmail.com>
2020-12-07 20:03:08 -0500

    Replace the App function to App class extends Componet

--- mosters-rolodex/src/App.js
+++ mosters-rolodex/src/App.js
@@ -1,25 +1,27 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello my name is Kender
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      string: 'Hello Kender Bolivar'
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            { this.state.string }
+          </p>
+          <button onClick={() => this.setState({ string: 'Hello Nayleth'})}>Change Text</button>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
