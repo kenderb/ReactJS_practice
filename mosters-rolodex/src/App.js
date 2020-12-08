c72b05e07fbfc2d71091ebaf7b3b17cde4910b44
kenderb <kenderbolivar@gmail.com>
2020-12-07 20:48:00 -0500

    Fetch and api of user to fill the array of monsters

--- mosters-rolodex/src/App.js
+++ mosters-rolodex/src/App.js
@@ -1,28 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        {
-          name:'Frankenstein',
-          id: 'asc1'
-        },
-        {
-          name:'Dracula',
-          id: 'asc2'
-        },
-        {
-          name:'Zombie',
-          id: 'asc3'
-        }
-      ]
+      monsters: []
     };
   }
-
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users}))
+  }
   render() {
     return (
       <div className="App">
