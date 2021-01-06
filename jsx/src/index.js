1185934d9c9d75defeac2d615768073435b10f60
kenderb <40863532+kenderb@users.noreply.github.com>
2021-01-06 14:08:24 -0500

    Add a basic Hello world to the DOM with React

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -3,5 +3,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 //Create a React component
+const App =  () => {
+  return <h1>Hello world.</h1>;
+};
 
 // Take the React component and show it on the screen
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+);
