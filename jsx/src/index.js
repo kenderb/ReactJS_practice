30e23c00a4e14f0ef9bccebdd791a765f11fc365
kenderb <40863532+kenderb@users.noreply.github.com>
2021-01-06 17:42:25 -0500

    Import React and ReactDOM to the index.js

---
+++ jsx/src/index.js
@@ -0,0 +1,7 @@
+// Import the React and ReactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//Create a React component
+
+// Take the React component and show it on the screen
