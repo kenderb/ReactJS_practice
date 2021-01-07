f963eb55194f24ce2ec70d27be2a2934aeada2ea
kenderb <40863532+kenderb@users.noreply.github.com>
2021-01-06 20:09:43 -0500

    Add basic styling to the app

--- components/src/index.js
+++ components/src/index.js
@@ -3,8 +3,25 @@ import ReactDOM from 'react-dom';
 
 const App = () => {
   return (
-    <div>
-      Hello!!!
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img  alt="avatar" />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">
+              Today at 10:00PM.
+            </span>
+          </div>
+          <div className="text">
+            Nice app here, men!
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
