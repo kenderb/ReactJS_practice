00287eb6088d83c0c0b3d680dd5aec2a17155488
kenderb <40863532+kenderb@users.noreply.github.com>
2021-01-06 21:49:39 -0500

    Add faker with random image

--- components/src/index.js
+++ components/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import avatar from './images/faces/ceekaytweet_128.jpg'
+import faker from 'faker';
 
 const App = () => {
   return (
     <div className="ui container comments">
       <div className="comment">
         <a href="/" className="avatar">
-          <img  alt="avatar" src={avatar} />
+          <img  alt="avatar" src={faker.image.imageUrl()} />
         </a>
         <div className="content">
           <a href="/" className="author">
