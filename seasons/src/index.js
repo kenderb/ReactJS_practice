570ac4f7c88578342b51a4ab003066dd97c56dea
kenderb <kenderbolivar@gmail.com>
2021-01-26 15:18:17 -0500

    Refactor the App component

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDsiplay from './SeasonDisplay';
-
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => position.coords,
-    (error) => console.log(error)
-  );
-  return(
-    <div>
-      Latitud:
-    </div>
-  );
-};
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => position.coords,
+      (error) => console.log(error)
+    );
+    return(
+      <div>
+        Latitud:
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
   <App />,
