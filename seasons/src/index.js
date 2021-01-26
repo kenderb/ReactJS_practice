d094d1e5027763ecbd81b8ee0da004279f2c8582
kenderb <kenderbolivar@gmail.com>
2021-01-26 15:48:26 -0500

    Update the state using setState

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -5,17 +5,20 @@ class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { latitude: null};
+    this.state = { latitude: null };
+  
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ latitude: position.coords.latitude })
+      },
+      (error) => console.log(error)
+    );
   }
 
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => position.coords,
-      (error) => console.log(error)
-    );
     return(
       <div>
-        Latitud:
+        Latitud: { this.state.latitude }
       </div>
     );
   }
