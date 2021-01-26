5ba9f3fb917b33833698d6b2df3cb3a685183b92
kenderb <kenderbolivar@gmail.com>
2021-01-26 17:44:48 -0500

    Add logic to calculate the weather

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
+const getSeason = (latitude, month) => {
+  if (month > 2 && month < 9) {
+    return latitude > 0 ? 'Summer' : 'Winter';
+  } else {
+    return latitude > 0 ? 'Winter' : 'Summer';
+  }
+}
+
 const SeasonDisplay = (props) => {
+  const season = getSeason(props.latitude, new Date().getMonth());
   return(
-    <div>Season here</div>
+    <div>Season here { season }</div>
   );
 }
 
