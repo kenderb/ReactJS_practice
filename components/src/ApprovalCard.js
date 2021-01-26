0b9afec2fb0f0ff601d5627b575f82b594b8836d
kenderb <kenderbolivar@gmail.com>
2021-01-26 13:17:50 -0500

    Add the CommentDatil to the ApprovalCard component

--- components/src/ApprovalCard.js
+++ components/src/ApprovalCard.js
@@ -4,7 +4,7 @@ const ApprovalCard = props => {
   return (
     <div className="ui card">
       <div className="content">
-        Are you sure?
+        {props.children}
       </div>
       <div className="extra content">
         <div className="ui two buttons">
--- components/src/index.js
+++ components/src/index.js
@@ -6,22 +6,27 @@ import ApprovalCard from "./ApprovalCard";
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDatil
-        author="Kender"
-        date="9:00 PM"
-        comment="Hi there people"
-      />
-      <CommentDatil
-        author="Keelan"
-        date="5:00 AM"
-        comment="OHH noooo!!"
-      />
-      <CommentDatil
-        author="Kenay"
-        date="3:00 PM"
-        comment="Where is my food"
-      />
+      <ApprovalCard>
+        <CommentDatil
+          author="Kender"
+          date="9:00 PM"
+          comment="Hi there people"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDatil
+          author="Keelan"
+          date="5:00 AM"
+          comment="OHH noooo!!"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDatil
+          author="Kenay"
+          date="3:00 PM"
+          comment="Where is my food"
+        />
+      </ApprovalCard>
     </div>
   );
 }
