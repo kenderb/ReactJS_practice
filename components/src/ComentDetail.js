e269873e69b0badf71d18e6eceecadf5772c10e3
kenderb <40863532+kenderb@users.noreply.github.com>
2021-01-25 23:01:49 -0500

    Add the comment detail component

---
+++ components/src/ComentDetail.js
@@ -0,0 +1,5 @@
+import React from 'react';
+const CommentDetail = () => {
+  
+}
+export default CommentDetail;
--- components/src/index.js
+++ components/src/index.js
@@ -7,7 +7,7 @@ const App = () => {
     <div className="ui container comments">
       <div className="comment">
         <a href="/" className="avatar">
-          <img  alt="avatar" src={faker.image.imageUrl()} />
+          <img  alt="avatar" src={faker.image.image()} />
         </a>
         <div className="content">
           <a href="/" className="author">
