3e6fca1e64f8ce313931fe990d8331c4a3717c28
kenderb <kenderbolivar@gmail.com>
2021-01-26 16:38:00 -0500

    Add some error handeleer to the App

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -5,20 +5,29 @@ class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { latitude: null };
+    this.state = { latitude: null, errorMessage: '' };
   
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         this.setState({ latitude: position.coords.latitude })
       },
-      (error) => console.log(error)
+      error => this.setState({ errorMessage: error.message })
+    );
+  }
+
+  showErrormessage(){
+    return(
+      <div>
+        Error: {this.state.errorMessage}
+      </div>
     );
   }
 
   render() {
+    if (this.state.errorMessage) return this.showErrormessage();
     return(
       <div>
-        Latitud: { this.state.latitude }
+        Latitud: { this.state.latitude } <br/>
       </div>
     );
   }
