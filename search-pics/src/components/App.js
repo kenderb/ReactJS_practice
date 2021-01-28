3fa74609920135da50db1bd3e2e0abf5bd2e6933
kenderb <40863532+kenderb@users.noreply.github.com>
2021-01-28 17:45:19 -0500

    Refactor App component

---
+++ search-pics/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class App extends React.Component {
+
+  render() {
+    return(
+      <div>
+        <h1>
+          Hello there
+        </h1>
+      </div>
+    );
+  }
+}
--- search-pics/src/index.js
+++ search-pics/src/index.js
@@ -1,16 +1,6 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App'
 
-class App extends React.Component {
-  render(){
-    return(
-      <div>
-        <h1>
-          Hello there
-        </h1>
-      </div>
-    );
-  }
-}
+
 
 ReactDOM.render(<App />, document.querySelector('#root'));
