9bfbb1564fd3a2e42c0af2abf867d314014ac84f
kenderb <40863532+kenderb@users.noreply.github.com>
2021-01-29 15:34:37 -0500

    Refactor the dinamic class creation on the Counter Component

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -5,13 +5,19 @@ class Counter extends Component {
     count: 0
   };
 
-  render() { 
+  render() {
     return (
       <div>
-        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classess = 'badge m-2 badge-';
+    classess += this.state.count === 0 ? 'warning' : 'primary';
+    return classess;
   }
 
   formatCount() {
