9f1d06c353296f8f42e870164a8ede67c35cfe10
kenderb <40863532+kenderb@users.noreply.github.com>
2021-01-29 16:55:13 -0500

    Add a arrow funtion to the onClick event to pass a value as an argument

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -6,24 +6,21 @@ class Counter extends Component {
     tags: ["tag1", "tag2", "tag3"]
   };
 
-  handleIncrement = () => {
+  handleIncrement = (product) => {
+    console.log(product);
     this.setState({ count: this.state.count + 1 });
   }
 
   render() {
-    const { tags } = this.state
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button 
-          onClick={this.handleIncrement} 
+          onClick={() => this.handleIncrement('My product ')} 
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
-        {/* <ul>
-          {tags.map(tag => <li key={tag}>{tag}</li>)}
-        </ul> */}
       </div>
     );
   }
