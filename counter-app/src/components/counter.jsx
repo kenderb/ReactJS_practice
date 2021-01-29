c223450102f4b6d92f0de4b66acabe87f7dc3367
kenderb <40863532+kenderb@users.noreply.github.com>
2021-01-29 16:42:59 -0500

    Some refactor of the Increment method

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -7,11 +7,7 @@ class Counter extends Component {
   };
 
   handleIncrement = () => {
-    this.setState(
-      {
-        count: this.state.count + 1
-      }
-    );
+    this.setState({ count: this.state.count + 1 });
   }
 
   render() {
