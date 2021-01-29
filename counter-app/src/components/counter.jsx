af96eec169342198b64331eb9521fb6fe255ac35
kenderb <40863532+kenderb@users.noreply.github.com>
2021-01-29 16:42:16 -0500

    Increment the count using setState

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -7,7 +7,11 @@ class Counter extends Component {
   };
 
   handleIncrement = () => {
-    console.log('Increment Clicked', this);
+    this.setState(
+      {
+        count: this.state.count + 1
+      }
+    );
   }
 
   render() {
