4443fc27ad0d3e29b548e28673a830ad2f2e7388
kenderb <40863532+kenderb@users.noreply.github.com>
2021-01-29 15:44:00 -0500

    Add a list of li with map

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,14 +2,19 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
   };
 
   render() {
+    const { tags } = this.state
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {tags.map(tag => <li key={tag}>{tag}</li>)}
+        </ul>
       </div>
     );
   }
