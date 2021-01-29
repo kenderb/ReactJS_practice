0e9a75c698a011bb3ea666f8ab35d1dfae7f29f6
kenderb <40863532+kenderb@users.noreply.github.com>
2021-01-29 15:20:44 -0500

    Add some bootstrpa classes to the Counter component

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -4,11 +4,12 @@ class Counter extends Component {
   state = {
     count: 0
   };
+
   render() { 
     return (
       <div>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
@@ -18,5 +19,5 @@ class Counter extends Component {
     return count === 0 ? 'Zero' : count;
   }
 }
- 
+
 export default Counter;
