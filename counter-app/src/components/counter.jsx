b67b81528ce159b05664ae3cd02a12508c5ccfd4
kenderb <40863532+kenderb@users.noreply.github.com>
2021-01-29 16:36:48 -0500

    Add arrow funtion to the Increment funtion to avoid binding the parent object

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -6,8 +6,8 @@ class Counter extends Component {
     tags: ["tag1", "tag2", "tag3"]
   };
 
-  handleIncrement(){
-    console.log('Increment Clicked');
+  handleIncrement = () => {
+    console.log('Increment Clicked', this);
   }
 
   render() {
