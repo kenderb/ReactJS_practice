976e827c4305767c39ee312dd3c2e7f48a66af60
kenderb <40863532+kenderb@users.noreply.github.com>
2021-02-02 16:31:56 -0500

    Fix some typos

--- widgets-app/src/App.js
+++ widgets-app/src/App.js
@@ -5,17 +5,17 @@ const items = [
   {
     id: 1,
     title: 'What is python?',
-    description: 'Python is a programming lenguaje to create great apps.'
+    description: 'Python is a programming language to create great apps.'
   },
   {
     id: 2,
     title: 'What is React?',
-    description: 'Is a fornt-end libery base on components.'
+    description: 'Is a front-end library based on components.'
   },
   {
     id: 3,
     title: 'What is JavaScript',
-    description: 'JavaScript is a programming lenguaje to create great apps using HTML and CSS.'
+    description: 'JavaScript is a programming language to create great apps using HTML and CSS.'
   },
 ];
 const App = () => {
