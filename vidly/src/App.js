4059aa9cd47d29afb3733b96d7793dcbbca94e8e
kenderb <40863532+kenderb@users.noreply.github.com>
2021-02-01 19:37:37 -0500

    Add the pagination Component

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Movies from './components/Movies';
+import Pagination from './components/Pagination';
 
 class App extends Component {
   render() { 
     return ( 
       <div className="container">
         <Movies />
+        <Pagination />
       </div>
     );
   }
