7afa0d295c115c0dc39678a86e5ef9660782436b
kenderb <40863532+kenderb@users.noreply.github.com>
2021-02-03 10:09:44 -0500

    Add the useState to the Acordion component

--- widgets-app/src/components/Accordion.js
+++ widgets-app/src/components/Accordion.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSortDown } from '@fortawesome/free-solid-svg-icons'
 
-const renderTitle = (title) =>{
+
+const handleClick = (index, setActiveIndex) => {
+  setActiveIndex(index);
+}
+
+const renderTitle = (title, index, setActiveIndex) => {
   return(
     <div className="card-header">
       <h5 className="mb-0">
-        <button className="btn btn-link collapsed" >
-          {title}
+        <button className="btn btn-link collapsed" onClick={() => handleClick(index, setActiveIndex)}>
+          <FontAwesomeIcon icon={faSortDown} /> {title}
         </button>
       </h5>
     </div>
@@ -23,11 +30,12 @@ const renderDescription = (description) => {
 }
 
 const Accordion = ({items}) => {
+  const [activeIndex, setActiveIndex] = useState(null);
 
   const listOfItems = items.map( item => {
     return(
       <div className="card" key={item.id}>
-        {renderTitle(item.title)}
+        {renderTitle(item.title, item.id, setActiveIndex)}
         {renderDescription(item.description)}
       </div>
     );
@@ -36,6 +44,7 @@ const Accordion = ({items}) => {
   return(
     <div className="accordion">
       { listOfItems }
+      { activeIndex }
     </div>
   );
 }
