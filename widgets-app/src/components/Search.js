bbaf7ce3df7153ba91bf7c45e9a1a5cf8a69aeeb
kenderb <40863532+kenderb@users.noreply.github.com>
2021-02-03 11:22:33 -0500

    Add some style to the Search component

--- widgets-app/src/components/Search.js
+++ widgets-app/src/components/Search.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 const Search = () => {
   return(
-    <div>Hi from search</div>
+    <form className="card container">
+      <div className="form-group card-body">
+        <label htmlFor="name" className="font-weight-bold">Search a topic </label>
+        <input type="text" id="name" placeholder="programming" className="form-control "/>
+      </div>
+    </form>
   );
 }
 
