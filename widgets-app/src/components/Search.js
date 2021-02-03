92912dc11d9111dc9957c67b4251052133243562
kenderb <40863532+kenderb@users.noreply.github.com>
2021-02-03 11:37:37 -0500

    Add some events to the Search component

--- widgets-app/src/components/Search.js
+++ widgets-app/src/components/Search.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+
+const handleChange = (event, setText) => {
+  setText(event.target.value);
+};
+
+const handleSubmit = (event, text) => {
+  event.preventDefault();
+  console.log('submited', text);
+};
 
 const Search = () => {
+  const [text, setText] = useState(' ');
+
   return(
-    <form className="card container">
+    <form onSubmit = {(e) => handleSubmit(e, text)} className="card container">
       <div className="form-group card-body">
         <label htmlFor="name" className="font-weight-bold">Search a topic </label>
-        <input type="text" id="name" placeholder="programming" className="form-control "/>
+        <input 
+          type="text" 
+          id="name" 
+          placeholder="programming" 
+          className="form-control"
+          onChange={(e) => handleChange(e, setText)}
+          value = {text}
+        />
       </div>
     </form>
   );
