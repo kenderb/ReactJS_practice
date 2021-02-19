42f4ae411d812080b5ba342bb4b2c1b94fcaf3b5
kenderb <40863532+kenderb@users.noreply.github.com>
2021-02-19 00:18:01 -0500

    Add the basic structure of the app and boostrap

---
+++ redux-thunk-practice/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Card} from 'react-bootstrap';
+
+const App = () => (
+  <Card>
+    <Card.Body> 
+      App
+    </Card.Body> 
+  </Card>
+);
+
+export default App;
