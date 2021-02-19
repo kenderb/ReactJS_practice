f75e78053a8b41f4f7e97e23e45b626e8a72fa4f
kenderb <kenderbolivar@gmail.com>
2021-02-19 14:01:44 -0500

    Add some styling

--- redux-thunk-practice/src/components/PostList.js
+++ redux-thunk-practice/src/components/PostList.js
@@ -1,16 +1,26 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import {Card} from 'react-bootstrap';
 import { fetchPosts } from '../actions';
 
 const PostList = ({ posts, fetchPosts }) => {
 
   useEffect(() => {
     fetchPosts();
-  },[]);
-
+  },[fetchPosts]);
+  console.log(posts);
   return (
     <div>
-      {posts.map(post => <div key={post.id}>{post.title}</div>)}
+      {posts.map(post => 
+        <Card key={post.id} style={{margin: '1rem'}}>
+          <Card.Body>
+            <Card.Title as="h3">{post.title}</Card.Title>
+            <Card.Text>
+              {post.body}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      )}
     </div>)
 }
 
--- redux-thunk-practice/src/reducers/postsReducer.js
+++ redux-thunk-practice/src/reducers/postsReducer.js
@@ -2,11 +2,9 @@ import { FETCH_POSTS } from '../actions';
 
  const postsReducer = (state = [], action) => {
   switch (action.type) {
-
     case FETCH_POSTS:
       return action.payload;
     default:
-      
       return state;
   }
 }
